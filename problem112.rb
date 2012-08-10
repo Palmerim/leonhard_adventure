8437e50adceb761618f03f995eb6a9c4f69d2e72
Sam H <heiniths@gmail.com>
2012-08-10 11:27:25 -0700

    Moved testing for bouncy numbers into second function.

--- problem112.rb
+++ problem112.rb
@@ -4,25 +4,9 @@ def solve112(target_proportion)
     num = 0
     while bouncy_proportion < target_proportion
         num += 1
-        increasing, decreasing = true, true
-        current = num
-        last = current % 10
-        while current > 0
-            current, digit = current.div(10), current % 10
-            if digit > last
-                #puts "decreasing from left to right#{digit} > #{last}"
-                increasing = false
-            elsif last > digit
-                #puts "increasing left to right #{digit} < #{last}"
-                decreasing = false
-            end
-            last = digit
-        end
-
-        if (!increasing && !decreasing)# || (increasing && decreasing)
+        if bouncy?(num)
             total_bouncy += 1
             bouncy_proportion = 1.0*total_bouncy/num
-            #puts "#{num} bouncy #{bouncy_proportion}"
         end
     end
     puts target_proportion, num
@@ -45,14 +29,8 @@ def bouncy?(current)
             return true
         else
             return false
-end
-=begin
-    for n in 0..max_num
-        n.to_s.split.each do |digit|
-            puts digit
         end
-    end
-=end
-
+end
 
 solve112(0.99)
+
